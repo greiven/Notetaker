5b49d34ae41375fb750d941f12422f47253fd14b
Greivin <greiven@Greivins-MacBook-Pro.local>
2020-04-03 02:15:40 -0700

    2

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,12 @@
 var path = require("path");
-// configuring routes for HTTP requests.
 module.exports = function (app) {
-// return notes.html file
     app.get("/notes", (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
-// return index.html file
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
-    // If no matching route is found default to home
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
+
 const path = require("path");
 var express = require("express");
-// create express app
 var app = express();
-// Sets an initial port. We"ll use this in our listener
 var PORT = process.env.PORT || 3030;
- let noteCount = 1;
-// Sets up the Express app to handle data parsing
+let noteCount = 1;
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// static files
 app.use(express.static(path.join(__dirname, "public")));
-// create a controller for our note app so we can handle the routes and the rendering of
-// views then require the controllers (modules)
+
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 
-// Start the server & listen for requests
 app.listen(PORT, function() {
     console.log('server is running and listening on http://localhost:' + PORT)
 });
