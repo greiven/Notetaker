2432ee3be991215526d5294d27829de583c3cee2
Greivin <greiven@Greivins-MacBook-Pro.local>
2020-04-07 15:30:32 -0700

    fixing

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-
 const path = require("path");
+
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3030;
+
+
 let noteCount = 1;
 
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-require("./routes/api-routes")(app);
-require("./routes/html-routes")(app);
-
 app.listen(PORT, function() {
-    console.log('server is running and listening on http://localhost:' + PORT)
+console.log('server is running and listening on http://localhost:' + PORT)
 });
